7c2ed327dfe11ee682e943f25ec161da03096b1f
BeejLuig <bjcantlupe@gmail.com>
2017-06-04 12:07:38 -0400

    Create initial Team class

---
+++ lib/soccer_league/team.rb
@@ -0,0 +1,19 @@
+class Team
+  attr_accessor :name, :points
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_or_create_by_name(name)
+    team = self.all.bsearch {|team| name == team.name}
+    team.nil? ? self.new(name) : team
+  end
+end
