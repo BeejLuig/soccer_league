0cff37db3997f31ba05938c56dfa8035b1350f41
BeejLuig <bjcantlupe@gmail.com>
2017-06-04 12:49:22 -0400

    build parse method

--- lib/soccer_league/parser.rb
+++ lib/soccer_league/parser.rb
@@ -6,7 +6,11 @@ class Parser
   def parse(file_path)
     file = File.new(file_path)
     file.each do |line|
-      puts line
+      teams = line.split(", ")
+      team_one_with_score = teams[0].split(" ")
+      team_two_with_score = teams[1].split(" ")
+      game = Game.new(team_one_with_score, team_two_with_score)
+      game.play
     end
   end
 
