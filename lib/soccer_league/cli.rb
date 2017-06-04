a817cf3dea789ab0e96c1702167ee0333a97597f
BeejLuig <bjcantlupe@gmail.com>
2017-06-04 19:00:35 -0400

    refactor display_league_rankings in cli.rb

--- lib/soccer_league/cli.rb
+++ lib/soccer_league/cli.rb
@@ -18,7 +18,7 @@ class CLI
   end
 
   private
-
+  
   def handle_input
     puts "Please enter the full pathname to your input text file"
     puts "Type 'q' to quit"
@@ -31,11 +31,12 @@ class CLI
     previous_rank = 0
     league_rankings.each_with_index do |team, i|
       if i > 0 && team.points == league_rankings[i-1].points
-        puts "#{previous_rank}. #{team.name}, #{team.points} pts"
+        rank = previous_rank
       else
-        puts  "#{i+1}. #{team.name}, #{team.points} pts"
+        rank = i+1
         previous_rank = i+1
       end
+      puts "#{rank}. #{team.name}, #{team.points} #{team.points == 1 ? 'pt':'pts'}"
     end
   end
 
