420ad460ba12a5ab5f3325bf6531952f19e27a29
BeejLuig <bjcantlupe@gmail.com>
2017-06-04 14:47:17 -0400

    add .parse_games and adjust initialize in game.rb

--- lib/soccer_league/game.rb
+++ lib/soccer_league/game.rb
@@ -1,24 +1,31 @@
 class Game
 
   def initialize(team_one_with_score, team_two_with_score)
-    @team_one = Team.find_or_create_by_name(team_one_with_score[0])
-    @team_two = Team.find_or_create_by_name(team_two_with_score[0])
+    @team_one_score = team_one_with_score.pop.to_i
+    @team_two_score = team_two_with_score.pop.to_i
 
-    @team_one_score = team_one_with_score[1].to_i
-    @team_two_score = team_two_with_score[1].to_i
+    @team_one = Team.find_or_create_by_name(team_one_with_score.join(" "))
+    @team_two = Team.find_or_create_by_name(team_two_with_score.join(" "))
   end
 
   def play
     if @team_one_score == @team_two_score
       @team_one.points += 1
       @team_two.points += 1
-      puts "It's a tie!"
     elsif @team_one_score > @team_two_score
       @team_one.points += 3
-      puts "#{@team_one.name} win!"
     else
       @team_two.points += 3
-      puts "#{@team_two.name} win!"
+    end
+  end
+
+  def self.parse_games(file)
+    file.each do |line|
+      teams = line.split(", ")
+      team_one_with_score = teams[0].split(" ")
+      team_two_with_score = teams[1].split(" ")
+      game = self.new(team_one_with_score, team_two_with_score)
+      game.play
     end
   end
 end
