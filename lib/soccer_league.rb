345e6c79a61d03e2c2bcb8e6e06a9f0b5b9ef4ed
BeejLuig <bjcantlupe@gmail.com>
2017-06-04 14:46:34 -0400

    update "require" to "require_relative"

--- lib/soccer_league.rb
+++ lib/soccer_league.rb
@@ -1,6 +1,5 @@
-require "soccer_league/team"
-require "soccer_league/game"
-require "soccer_league/version"
+require_relative "../lib/soccer_league/team"
+require_relative "../lib/soccer_league/game"
+require_relative "../lib/soccer_league/version"
 
-require_relative "../lib/soccer_league/parser"
 require_relative "../lib/soccer_league/cli"
