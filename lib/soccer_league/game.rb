3b8d3d1322b4786a5e3c4985e807cd79a93c2200
BeejLuig <bjcantlupe@gmail.com>
2017-06-04 12:45:00 -0400

    build Game#initialize and Game#play methods

--- lib/soccer_league/game.rb
+++ lib/soccer_league/game.rb
@@ -1,5 +1,24 @@
 class Game
-  def initialize(team_one, score_one, team_two, score_two)
 
+  def initialize(team_one_with_score, team_two_with_score)
+    @team_one = Team.find_or_create_by_name(team_one_with_score[0])
+    @team_two = Team.find_or_create_by_name(team_two_with_score[0])
+
+    @team_one_score = team_one_with_score[1].to_i
+    @team_two_score = team_two_with_score[1].to_i
+  end
+
+  def play
+    if @team_one_score == @team_two_score
+      @team_one.points += 1
+      @team_two.points += 1
+      puts "It's a tie!"
+    elsif @team_one_score > @team_two_score
+      @team_one.points += 3
+      puts "#{@team_one.name} win!"
+    else
+      @team_two.points += 3
+      puts "#{@team_two.name} win!"
+    end
   end
 end
