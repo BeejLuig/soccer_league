ed90fdd4452266e6bf2aff682799440e03004218
BeejLuig <bjcantlupe@gmail.com>
2017-06-05 19:56:33 -0400

    re-order require statements and add pry (commented)

--- lib/soccer_league.rb
+++ lib/soccer_league.rb
@@ -1,5 +1,7 @@
-require_relative "../lib/soccer_league/team"
-require_relative "../lib/soccer_league/game"
-require_relative "../lib/soccer_league/version"
+# Uncomment to use binding.pry in lib files
+# require "pry"
 
 require_relative "../lib/soccer_league/cli"
+require_relative "../lib/soccer_league/game"
+require_relative "../lib/soccer_league/team"
+require_relative "../lib/soccer_league/version"
